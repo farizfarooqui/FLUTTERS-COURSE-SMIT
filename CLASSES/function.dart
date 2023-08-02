1558e1fa09714205fd9d42ed96e12eee4cfb36b4
farizfarooqui <farizfarooqui104@gmail.com>
2023-08-02 15:33:57 +0500

    update

--- CLASSES/function.dart
+++ CLASSES/function.dart
@@ -1,48 +1,47 @@
 import 'dart:io';
 
 void main() {
-
-  MarkSheet('faaaas',45,67,89,98,76,78);
-
+  
+  MarkSheet();
 }
 
-MarkSheet(var name , var Subject1,var Subject2,var Subject3,var Subject4,var Subject5,var Subject6) {
+ MarkSheet() {
   stdout.write('Please Enter your Name : ');
-  name = stdin.readLineSync();
+  var name = stdin.readLineSync()!;
   stdout.write('Please Enter your First Subject Marks : ');
-  Subject1 = stdin.readLineSync();
+  var Subject1 = int.parse(stdin.readLineSync()!);
   stdout.write('Please Enter your Second Subject Marks : ');
-  Subject2 = stdin.readLineSync();
+  var Subject2 = int.parse(stdin.readLineSync()!);
   stdout.write('Please Enter your Third Subject Marks : ');
-  Subject3 = stdin.readLineSync();
+  var Subject3 = int.parse(stdin.readLineSync()!);
   stdout.write('Please Enter your Fourth Subject Marks : ');
-  Subject4 = stdin.readLineSync();
+  var Subject4 = int.parse(stdin.readLineSync()!);
   stdout.write('Please Enter your fifth Subject Marks : ');
-  Subject5 = stdin.readLineSync();
+  var Subject5 = int.parse(stdin.readLineSync()!);
   stdout.write('Please Enter your sixth Subject Marks : ');
-  Subject6 = stdin.readLineSync();
+  var Subject6 = int.parse(stdin.readLineSync()!);
   var Grade;
   var percentage = ((Subject1+Subject2+Subject3+Subject4+Subject5+Subject6)/6);
   print('$name : $percentage');
   if (percentage >= 90) {
     Grade = 'A';
-    print(Grade);
+    print('Grade : $Grade');
   }
   else if (percentage<90 && percentage>=80) {
     Grade = 'B';
-    print(Grade);
+    print('Grade : $Grade');
   }
   else if (percentage<80 && percentage>=70) {
     Grade = 'C';
-    print(Grade);
+    print('Grade : $Grade');
   }
-  else if (percentage<70 && percentage<=60) {
+  else if (percentage<70 && percentage>=60) {
     Grade = 'D';
-    print(Grade);
+    print('Grade : $Grade');
   }
-  else if (percentage<60 && percentage<=50) {
+  else if (percentage<60 && percentage>=50) {
     Grade = 'D';
-    print(Grade);
+    print('Grade : $Grade');
   }
   else {
     Grade = 'F';
