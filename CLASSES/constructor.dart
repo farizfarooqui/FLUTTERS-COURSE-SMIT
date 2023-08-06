dd28b64af5a131e0c9f40bdf85209ec3aec4e4bf
farizfarooqui <farizfarooqui104@gmail.com>
2023-08-06 14:57:28 +0500

    update

--- CLASSES/constructor.dart
+++ CLASSES/constructor.dart
@@ -1,59 +1,19 @@
-class Person {
-  String name;
-  int age;
-
-  // Default constructor
-  Person(this.name, this.age);
-
-  // Named constructor
-  factory Person.fromBirthYear(String name, int birthYear) {
-    int currentYear = DateTime.now().year;
-    int age = currentYear - birthYear;
-    return Person(name, age);
-  }
-
-  void introduce() {
-    print("Hi, my name is $name, and I am $age years old.");
-  }
+void main () {
+  print('Hello World!');
+  person person1 = person(Name: 'Fariz',Age : 44);
+  person1.View();
+  person person2 = person(Age: 76);
+  person2.View();
+  
 }
 
-void main() {
-  // Using the default constructor
-  var person1 = Person("Alice", 30);
-  person1.introduce();
+class person {
+  String Name;
+  int Age;
+  person({this.Name = 'John', this.Age=6});
 
-  // Using the named constructor
-  var person2 = Person.fromBirthYear("Bob", 1990);
-  person2.introduce();
+  View() {
+    print('Name is : $Name');
+    print('Age is : $Age');
+  }
 }
-
-// cop() {
-//   class Person {
-//   String name;
-//   int age;
-
-//   // Default constructor
-//   Person(this.name, this.age);
-
-//   // Named constructor
-//   Person.fromBirthYear(String name, int birthYear) {
-//     this.name = name;
-//     this.age = DateTime.now().year - birthYear;
-//   }
-
-//   void introduce() {
-//     print("Hi, my name is $name, and I am $age years old.");
-//   }
-// }
-
-// void main() {
-//   // Using the default constructor
-//   var person1 = Person("Alice", 30);
-//   person1.introduce();
-
-//   // Using the named constructor
-//   var person2 = Person.fromBirthYear("Bob", 1990);
-//   person2.introduce();
-// }
-
-// }
