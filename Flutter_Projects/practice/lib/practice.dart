b40a037f44607b080fff5387360d0dc137f3ba46
farizfarooqui <farizfarooqui104@gmail.com>
2023-08-28 00:52:38 +0500

    Updated

--- Flutter_Projects/practice/lib/practice.dart
+++ Flutter_Projects/practice/lib/practice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice/first_page.dart';
 
 class practive extends StatefulWidget {
   const practive({super.key});
@@ -11,6 +12,15 @@ class _practiveState extends State<practive> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home page'),),
+
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> firstPage()));
+        }, 
+        child: Text('Go to first page')),
+      ),
+    );
   }
 }
