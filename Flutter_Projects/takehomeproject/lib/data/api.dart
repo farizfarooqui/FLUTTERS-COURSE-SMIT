89391e3def286a2c4b25ea2964349471aca18d52
farizfarooqui <farizfarooqui104@gmail.com>
2023-10-14 04:46:27 +0500

    Updated

--- Flutter_Projects/takehomeproject/lib/data/api.dart
+++ Flutter_Projects/takehomeproject/lib/data/api.dart
@@ -3,21 +3,16 @@ import 'package:http/http.dart' as http;
 import 'package:takehomeproject/models/nobel_prizess.dart';
 
 class Api {
-  static List<NobelPrize> nobelPrizes = [];
-
-  static Future<List<NobelPrize>?> fetchNobelPrizes() async {
+  static Future<List<NobelPrize>> fetchNobelPrizes() async {
     const url = 'https://irfangul92.github.io/index.json';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body.toString())['nobelPrizes'];
-      final result =
-          data.map((e) => NobelPrize.fromJson(e)).toList().cast<NobelPrize>();
-      nobelPrizes.addAll(result);
-      return nobelPrizes;
-    } 
-    else {
-      return [];
+      final data = jsonDecode(response.body)['nobelPrizes'];
+      final result = data.map<NobelPrize>((e) => NobelPrize.fromJson(e)).toList();
+      return result;
+    } else {
+      throw Exception('Failed to load Nobel Prizes');
     }
   }
 }
--- Flutter_Projects/takehomeproject/lib/screens/home_view.dart
+++ Flutter_Projects/takehomeproject/lib/screens/home_view.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:takehomeproject/data/api.dart';
-import 'package:takehomeproject/models/nobel_prizess.dart';
 import 'package:takehomeproject/screens/detail_view.dart';
+import '../models/nobel_prizess.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
 
   @override
-  _HomeViewState createState() => _HomeViewState();
+  State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  Future<List<NobelPrize>>? _future;
+
   @override
   void initState() {
     super.initState();
-    Api.fetchNobelPrizes();
+    _future = Api.fetchNobelPrizes();
+  }
+
+  Future<void> _refreshData() async {
+    setState(() {
+      _future = Api.fetchNobelPrizes();
+    });
   }
 
   @override
@@ -26,141 +34,132 @@ class _HomeViewState extends State<HomeView> {
         backgroundColor: Colors.amber,
         title: const Center(
           child: Text(
-            '  Nobel Prizes',
+            'Nobel Prizes',
             style: TextStyle(color: Colors.black),
           ),
         ),
         actions: [
           IconButton(
-            onPressed: () {
-              setState(() {
-                Api.fetchNobelPrizes();
-              });
-            },
-            icon: const Icon(
-              Icons.replay_rounded,
-              color: Colors.black,
-            ),
+            onPressed: _refreshData,
+            icon: const Icon(Icons.refresh, color: Colors.black),
           )
         ],
       ),
-      body: FutureBuilder<List<NobelPrize>?>(
-        future: Api.fetchNobelPrizes(),
+      body: FutureBuilder<List<NobelPrize>>(
+        future: _future,
         builder: (context, snapshot) {
+          //error handling
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.amber,
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (snapshot.hasData && snapshot.data != null) {
+            return const Center(child: CircularProgressIndicator(
+              color: Colors.amber,
+            ));
+          } 
+          else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } 
+          else if (snapshot.hasData) {
+            final nobelPrizes = snapshot.data!;
             return ListView.builder(
-                itemCount: Api.nobelPrizes.length,
-                itemBuilder: (context, index) {
-                  final nobelPrize = Api.nobelPrizes[index];
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => DetailView(
-                                    nobelPrize: nobelPrize,
-                                  )));
-                    },
-                    child: Container(
-                      width: double.maxFinite,
-                      margin: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              width: MediaQuery.of(context).size.width * 0.19,
-                              padding: const EdgeInsets.all(2),
-                              child: const CircleAvatar(
-                                child: Icon(
-                                  Icons.person,
-                                  size: 40,
-                                ),
-                              )),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Stack(
-                                children: [
-                                  SizedBox(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.80,
-                                    child: Text(
-                                      '${nobelPrize.awardYear}-${nobelPrize.category}',
-                                      style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600),
+              itemCount: nobelPrizes.length,
+              itemBuilder: (context, index) {
+                final nobelPrize = nobelPrizes[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => DetailView(nobelPrize: nobelPrize),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: double.maxFinite,
+                    margin: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.19,
+                          padding: const EdgeInsets.all(2),
+                          child: const CircleAvatar(
+                            child: Icon(
+                              Icons.person,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Stack(
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.80,
+                                  child: Text(
+                                    '${nobelPrize.awardYear}-${nobelPrize.category}',
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
-                                  const Positioned(
-                                    right: 0,
-                                    top: 0,
-                                    child: Icon(
-                                      Icons.arrow_forward_ios_sharp,
-                                      color: Colors.grey,
+                                ),
+                                const Positioned(
+                                  right: 0,
+                                  top: 0,
+                                  child: Icon(
+                                    Icons.arrow_forward_ios_sharp,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Date Awarded: ${nobelPrize.dateAwarded}',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            for (var laureate in nobelPrize.laureates ?? [])
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Laureate: ${laureate.knownName}',
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w300,
                                     ),
-                                  )
-                                ],
-                              ),
-                              Text(
-                                'Date Awarded:${nobelPrize.dateAwarded}',
-                                style: const TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.w400),
-                              ),
-                              for (var laureate in nobelPrize.laureates ?? [])
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Laureate: ${laureate.knownName}',
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Container(
+                                    constraints: BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width *
+                                              0.80,
+                                    ),
+                                    child: Text(
+                                      'Motivation: ${laureate.motivation}',
                                       style: const TextStyle(
                                         fontSize: 14,
-                                        fontWeight: FontWeight.w300,
+                                        fontWeight: FontWeight.w400,
                                       ),
                                     ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Container(
-                                      constraints: BoxConstraints(
-                                          maxWidth: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.80),
-                                      child: Text(
-                                        'Motivation: ${laureate.motivation} \n',
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              const SizedBox(
-                                height: 10,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                                  ),
+                                ],
+                              ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      ],
                     ),
-                  );
-                });
-          } 
-          else {
-            return const Center(
-              child: Text('No data available'),
+                  ),
+                );
+              },
             );
+          } else {
+            return const Center(child: Text('No data available'));
           }
         },
       ),
