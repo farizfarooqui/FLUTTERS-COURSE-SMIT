20d411bc84c8d30ff5edd956c0d64822f92a3757
farizfarooqui <farizfarooqui104@gmail.com>
2023-10-17 22:20:48 +0500

    Updated

--- Flutter_Projects/firebasefirst/lib/custom_storage.dart
+++ Flutter_Projects/firebasefirst/lib/custom_storage.dart
@@ -32,7 +32,7 @@ class _CustomStorageState extends State<CustomStorage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Firebase Storage")),
+        title: const Center(child: Text("Firebase Storagee")),
       ),
       body: SingleChildScrollView(
         child: Column(
